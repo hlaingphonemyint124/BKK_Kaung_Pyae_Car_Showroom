4e865c5c8325f13384d6b2cd6c66891c6e89b08f
YoonLae515 <laewah1234567@gmail.com>
2026-04-05 23:40:32 +0700

    update authService.js Login/ GetCurrent User/ Logout

--- src/features/auth/services/authService.js
+++ src/features/auth/services/authService.js
@@ -6,8 +6,14 @@ export const loginUser = async (userData) => {
   return response.data;
 };
 
-// REGISTER
-export const registerUser = async (userData) => {
-  const response = await api.post("/auth/register", userData);
+// GET CURRENT USER
+export const getCurrentUser = async () => {
+  const response = await api.get("/auth/me");
+  return response.data;
+};
+
+// LOGOUT
+export const logoutUser = async () => {
+  const response = await api.post("/auth/logout");
   return response.data;
 };
