8bffca4a459b6b515ff782f22c7ad438f87c2037
YoonLae515 <laewah1234567@gmail.com>
2026-04-05 23:42:42 +0700

    Fixed api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // change later
+  baseURL: "http://localhost:5000", // ✅ remove /api
+  withCredentials: true, // 🔥 VERY IMPORTANT
   headers: {
     "Content-Type": "application/json",
   },
